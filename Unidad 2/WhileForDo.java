c84d2ab43b99d8388ae375ce1bacb06cf5bbecc0
Mario Alberto Arias Carballo <133185908+Mario081022@users.noreply.github.com>
2023-06-29 16:51:21 -0600

    Add files via upload

---
+++ Unidad 2/WhileForDo.java
@@ -0,0 +1,27 @@
+package pkgwhile.pkgfor.pkgdo;
+
+public class WhileForDo {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("Numeros del 1 al 100 con WHILE");
+        int i=1;
+        while(i<=100){
+            System.out.println(i);
+            i++;
+       }
+       System.out.println("Numeros del 1 al 100 con DO");
+       int j=1;
+       do{
+            System.out.println(j);
+            j++;
+        }while(j<=100);
+       
+       System.out.println("Numeros del 1 al 100 con FOR");
+       for(int k=1; k<=100; k++){
+           System.out.println(k);
+       }
+    }
+    
+}

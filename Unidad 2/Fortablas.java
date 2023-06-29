658cfb588e3fac15425c99c769618e533a71b72c
Mario Alberto Arias Carballo <133185908+Mario081022@users.noreply.github.com>
2023-06-29 16:59:06 -0600

    Add files via upload

---
+++ Unidad 2/Fortablas.java
@@ -0,0 +1,20 @@
+package fortablas;
+
+public class Fortablas {
+
+    public static void main(String[] args) {
+       int i,j;
+       for(i=1; i<=10; i++){
+           System.out.println(">Tabla del " + i);
+           System.out.println("-----------");
+           
+           for(j=1; j<=10; j++){
+               System.out.println(i + "x" + j + " = " + j*i);
+           }
+       
+       }
+    
+    
+    }
+    
+}

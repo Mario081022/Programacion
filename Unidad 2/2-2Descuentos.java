a1d7beeb6783985fe7319bfd328af02203df2dbb
Mario Alberto Arias Carballo <133185908+Mario081022@users.noreply.github.com>
2023-06-29 17:19:09 -0600

    2-2Descuentos.java

--- Unidad 2/2-2Descuentos.java
+++ Unidad 2/2-2Descuentos.java
@@ -3,41 +3,32 @@ import java.util.Scanner;
 public class Descuentos {
 
     public static void main(String[] args) {
-    //Definicion de Variables.
-       double PV =1000;
-       Scanner entrada = new Scanner(System.in);
-       int descuento=0;
-       double preciofinal;
-       String codPromocion;
-       
-       //Entrada de Datos
-       System.out.println("Su total de compra es: $"+ PV + "\n");
-       System.out.println("Tines un codigo de descuento Escribelo:");
-       codPromocion = entrada.next();
-        codPromocion = codPromocion.toUpperCase();
-        
-        //Proceso.
-        
-        //Salida de Datos
-        
-        switch(codPromocion){
-            case  "PlatiOctubre":
-              descuento= 20;
-              break;
-            case  "SilverOctubre":  
-              descuento= 50;
-              break;
-            case "BronceOctubre":
-              descuento = 35;
-              break;
-            case "OroOctubre":
-               descuento =75;
-               break;
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el código: ");
+        String codigo = scanner.nextLine();
+
+        System.out.print("Ingrese el precio del artículo: $");
+        double precio = scanner.nextDouble();
+
+        double precioConDescuento = 0.0;
+
+        switch (codigo) {
+            case "DESC10":
+                precioConDescuento = precio * 0.9;
+                break;
+            case "DESC20":
+                precioConDescuento = precio * 0.8;
+                break;
+            case "DESC30":
+                precioConDescuento = precio * 0.7;
+                break;
             default:
-               System.out.println("Has introsucido un codigo no valido:"); 
+                System.out.println("Código inválido. No se aplica descuento.");
+                return;
         }
-        preciofinal = PV -descuento;
-    
+
+        System.out.println("Precio original: $" + precio);
+        System.out.println("Precio con descuento: $" + precioConDescuento);
     }
     
-}
+    }

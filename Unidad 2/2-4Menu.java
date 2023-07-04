2c9218fdab6966f449ccfa90fc1738c8f1ccc4da
Mario Alberto Arias Carballo <133185908+Mario081022@users.noreply.github.com>
2023-07-04 15:16:43 -0600

    2-4Menu.java

--- Unidad 2/2-4Menu.java
+++ Unidad 2/2-4Menu.java
@@ -1,18 +1,18 @@
-package pkg2.pkg3menu;
+package programselector;
 import java.util.Scanner;
 
 public class ProgramSelector {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Seleccione un programa:");
+
+        System.out.println("Seleccione una opción:");
         System.out.println("1. Programa de calificaciones");
         System.out.println("2. Programa de cajero automático");
         System.out.println("3. Programa de descuento");
-        System.out.print("Ingrese el número de programa que desea ejecutar: ");
-        int programa = scanner.nextInt();
-        
-        switch (programa) {
+        System.out.print("Ingrese el número de opción que desea ejecutar: ");
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
             case 1:
                 ejecutarProgramaCalificaciones();
                 break;
@@ -23,72 +23,66 @@ public class ProgramSelector {
                 ejecutarProgramaDescuento();
                 break;
             default:
-                System.out.println("Opción inválida. El programa seleccionado no existe.");
+                System.out.println("Opción inválida. La opción seleccionada no existe.");
                 break;
         }
     }
-    
-    public static void ejecutarProgramaCalificaciones() {
-        import java.util.Scanner;
 
-public class Calificaciones {
-    public static void main(String[] args) {
+    public static void ejecutarProgramaCalificaciones() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingresa la calificación: ");
+        System.out.println("Programa de calificaciones");
+        System.out.print("Ingrese la calificación: ");
         int calificacion = scanner.nextInt();
-        
-        String mensaje;
-        
+
+        String mensaje = null;
+
         switch (calificacion) {
             case 0:
-            case 1:
-                mensaje = "Muy mal";
-                break;
-            case 2:
-                mensaje = "Insuficiente";
-                break;
-            case 3:
-                mensaje = "Suficiente";
-                break;
-            case 4:
-                mensaje = "Bien";
-                break;
-            case 5:
-                mensaje = "Notable";
-                break;
-            case 6:
-                mensaje = "Sobresaliente";
-                break;
-            default:
-                mensaje = "Error de calificación";
-                break;
-        }
-        
+                    System.out.println("MUY MAL");
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("INSUFICIENTE");
+                    break;
+                case 6:
+                    System.out.println("SUFICIENTE");
+                    break;
+                case 7:
+                case 8:
+                    System.out.println("BIEN");
+                    break;
+                case 9:
+                    System.out.println("NOTABLE");
+                    break;
+                case 10:
+                    System.out.println("SOBRESALIENTE");
+                    break;
+                default:
+                    System.out.println("ERROR DE CALIFICACION");
+                    break;
+            }
+
         System.out.println("Resultado: " + mensaje);
     }
-}
-        System.out.println("Ejecutando programa de calificaciones...");
-    }
-    
-    public static void ejecutarProgramaCajeroAutomatico() {
-        import java.util.Scanner;
 
-public class CajeroAutomatico {
-    public static void main(String[] args) {
+    public static void ejecutarProgramaCajeroAutomatico() {
         Scanner scanner = new Scanner(System.in);
-        double saldo = 15,000.0;
-        
-        System.out.println("Bienvenido al Cajero Automático");
-        
+        double saldo = 1000.0;
+
+        System.out.println("Programa de cajero automático");
+
         while (true) {
             System.out.println("--------------------------------");
             System.out.println("Opciones disponibles:");
             System.out.println("1. Consultar saldo");
             System.out.println("2. Realizar retiro");
             System.out.println("3. Salir");
-            System.out.print("Ingresa el número de opción: ");
+            System.out.print("Ingrese el número de opción: ");
             int opcion = scanner.nextInt();
-            
+
             switch (opcion) {
                 case 1:
                     System.out.println("Saldo actual: $" + saldo);
@@ -96,34 +90,28 @@ public class CajeroAutomatico {
                 case 2:
                     System.out.print("Ingrese la cantidad a retirar: $");
                     double cantidadRetiro = scanner.nextDouble();
-                    
+
                     if (cantidadRetiro > saldo) {
                         System.out.println("Fondos insuficientes. No se puede realizar el retiro.");
                     } else {
                         saldo -= cantidadRetiro;
                         System.out.println("Retiro exitoso. Nuevo saldo: $" + saldo);
                     }
-                    
+
                     break;
                 case 3:
                     System.out.println("Gracias por utilizar el Cajero Automático. ¡Hasta pronto!");
                     return;
                 default:
-                    System.out.println("Opción inválida. Por favor, ingresa un número válido.");
+                    System.out.println("Opción inválida. Por favor, ingrese un número válido.");
                     break;
             }
         }
     }
-}
-        System.out.println("Ejecutando programa de cajero automático...");
-    }
-    
-    public static void ejecutarProgramaDescuento() {
-        import java.util.Scanner;
 
-public class AplicarDescuento {
-    public static void main(String[] args) {
+    public static void ejecutarProgramaDescuento() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Programa de descuento");
         System.out.print("Ingrese el código: ");
         String codigo = scanner.nextLine();
 
@@ -151,8 +139,6 @@ public class AplicarDescuento {
         System.out.println("Precio con descuento: $" + precioConDescuento);
     }
 }
-        System.out.println("Ejecutando programa de descuento...");
-    }
 }}    
 
     
